58aa547247cf85bb91c05c73d368e6e0f157682e
Jayash Raj Mudbhari <jayashraj@outlook.com>
2020-07-05 21:00:29 +1000

    Updates

---
+++ precache-manifest.70f4caef5c1572dc880ba4624ec3a084.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e659c0bdd9809017c6b6bfdb8bf0724c",
+    "url": "/drums/index.html"
+  },
+  {
+    "revision": "fd815f96d7ae964a11d6",
+    "url": "/drums/static/css/main.1ee29cad.chunk.css"
+  },
+  {
+    "revision": "6d1d70a0e3d0402c7866",
+    "url": "/drums/static/js/2.b7a23753.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drums/static/js/2.b7a23753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd815f96d7ae964a11d6",
+    "url": "/drums/static/js/main.f47e32c8.chunk.js"
+  },
+  {
+    "revision": "aa59b7696db63f717b0a",
+    "url": "/drums/static/js/runtime-main.5b5778aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/drums/static/media/logo.5d5d9eef.svg"
+  }
+]);
